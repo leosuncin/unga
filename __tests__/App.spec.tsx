e3a7d2d28d7d103ee33850de3fcd97005df68419
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-01-31 12:34:39 -0600

    chore: Initial commit

---
+++ __tests__/App.spec.tsx
@@ -0,0 +1,10 @@
+import 'react-native';
+import React from 'react';
+import App from '../App';
+
+// Note: test renderer must be required after react-native.
+import { render } from '@testing-library/react-native';
+
+it('renders correctly', () => {
+  expect(render(<App />)).toBeDefined();
+});
