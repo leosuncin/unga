d9f0c65a24d815774b77726dfa0f7ca12bc5eaee
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-02-03 15:30:25 -0600

    feat: Add start game screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Header from './components/header';
+import StartGameScreen from './screens/start-game';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header title="Guess a number" />
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <StartGameScreen />
     </View>
   );
 }
---
+++ screens/start-game.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Start a new game!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a number</Text>
+        <TextInput keyboardType="numeric" />
+        <View style={styles.buttonContainer}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 5,
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+  },
+});
+
+export default StartGameScreen;
