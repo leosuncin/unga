6c249e16aa2dec27b497f086b5e8afd2a79609fb
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-02-06 15:45:44 -0600

    feat: Load custom font

--- App.tsx
+++ App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { loadAsync } from 'expo-font';
+import { AppLoading } from 'expo';
+
+function loadResources() {
+  return loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+}
 
 export default function App() {
-  return (
+  const [loading, setLoading] = useState(true);
+
+  return loading ? (
+    <AppLoading
+      startAsync={loadResources}
+      onFinish={() => setLoading(false)}
+      onError={console.error}
+    />
+  ) : (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
